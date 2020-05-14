9378234da0da118327b17b9127bde8a5fa65a1e5
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-05-13 18:50:20 -0700

    Create Main.php

---
+++ src/HubCommand/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace HubCommand;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class Main extends PluginBase {
+
+    public function onEnable() : void {
+        $this->getLogger()->info(TextFormat::GREEN . "HubCommand has been enabled");
+    }
+
+    public function onDisable() : void {
+        $this->getLogger()->info(TextFormat::RED . "HubCommand has been disabled");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+        $spawnLocation = $this->getServer()->getDefaultLevel()->getSpawnLocation();
+        switch ($command->getName()) {
+            case "hub":
+            case "lobby":
+                if ($sender instanceof Player) {
+                    $sender->getPlayer()->teleport($spawn);
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Please use this command in-game");
+                }
+                return true;
+                break;
+        }
+        return false;
+    }
+
+}
